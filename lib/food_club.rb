3489c32ee7480be22e401d77bbabbd1c0ce0bd4f
Launch Academy <launchacademy@PresentationMachine.local>
2016-02-25 16:56:40 -0500

    completed during clinic

--- lib/food_club.rb
+++ lib/food_club.rb
@@ -1,2 +1,33 @@
 class FoodClub
+  attr_reader :name, :start_date, :end_date, :foodies, :theme_dinners
+
+  def initialize(name, start_date, end_date)
+    @name = name
+    @start_date = start_date
+    @end_date = end_date
+    @foodies = []
+    @theme_dinners = []
+  end
+
+  def grand_finale
+    @end_date - 1
+  end
+
+  def sign_up(foodie)
+    @foodies << foodie
+  end
+
+  def schedule(theme_dinner)
+    @theme_dinners << theme_dinner
+  end
+
+  def member_list
+    output = "\nFood Club: #{name}\n"
+    output += "-----------------\n"
+
+    @foodies.each do |foodie|
+      output += "#{foodie.name} #{foodie.email}\n"
+    end
+    output
+  end
 end
